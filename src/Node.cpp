9abebe8b14385220d6b08820daf443938d97e4bd
bpmn-os <bpmn-os@users.noreply.github.com>
2023-06-29 15:23:46 +0000

    deploy: 007ce90d38008b3139195463cd81a7bd7e888601

--- src/Node.cpp
+++
@@ -1,9 +0,0 @@
-#include "Node.h"
-#include "FlowNode.h"
-
-using namespace BPMN;
-
-Node::Node(XML::bpmn::tBaseElement* element, Scope* parent) : element(element), parent(parent)
-{
-  id = element->id.has_value() ? (std::string)element->id->get() : "";
-}
