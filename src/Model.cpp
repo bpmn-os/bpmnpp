bbe4f05f694fb87e3ded66a8c7a9c905290eb771
Asvin Goel <goel@telematique.eu>
2023-07-01 16:17:00 +0200

    Revert regression

--- src/Model.cpp
+++ src/Model.cpp
@@ -479,7 +479,7 @@ void Model::createReferences(FlowNode* flowNode) {
     // link outgoing sequence flows
     for ( auto& outflow : flowNode->element->outgoing ) {
       for (auto& sequenceFlow : flowNode->parent->sequenceFlows ) {
-        if ( outflow.get().textContent == (std::string)sequenceFlow->get()->id.value() ) {
+        if ( sequenceFlow->get()->id.has_value() && outflow.get().textContent == (std::string)sequenceFlow->get()->id->get() ) {
           flowNode->outgoing.push_back(sequenceFlow.get());
           break;
         }
