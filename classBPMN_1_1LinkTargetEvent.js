39a63ee35bad23b51e0d0da92c9cbb11ce092745
rajgoel <rajgoel@users.noreply.github.com>
2023-12-28 15:11:13 +0000

    deploy: 801624ffb3664075b5935fdbc8c0c2e0839f6fe0

--- classBPMN_1_1LinkTargetEvent.js
+++ classBPMN_1_1LinkTargetEvent.js
@@ -4,5 +4,5 @@ var classBPMN_1_1LinkTargetEvent =
     [ "Model", "classBPMN_1_1LinkTargetEvent.html#a2bf2a0e9b454c55aa5dcb5aa4698697b", null ],
     [ "element", "classBPMN_1_1LinkTargetEvent.html#a6fe9f14bd2084ccaa9124d27bce690a0", null ],
     [ "linkName", "classBPMN_1_1LinkTargetEvent.html#a785fe322ee8b3e7d7336f0e8126b5692", null ],
-    [ "source", "classBPMN_1_1LinkTargetEvent.html#a50c42d35d19015dc76cf3cf84ad08358", null ]
+    [ "sources", "classBPMN_1_1LinkTargetEvent.html#acc9690ec49e5d6dcdb998648231f3cd7", null ]
 ];
